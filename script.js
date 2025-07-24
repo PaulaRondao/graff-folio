03ef9034b5d779fa32f2e5fe184ce7f969c220e9
PaulaRondao <maud.rondao@gmail.com>
2025-07-24 19:17:17 +0800

    update navbar for more accessibility

--- script.js
+++ script.js
@@ -9,11 +9,34 @@ const collapse = () => {
   const isExpanded = button.getAttribute("aria-expanded") === "true";
   button.setAttribute("aria-expanded", (!isExpanded).toString());
 
+  document.body.classList.toggle("no-scroll", !isExpanded);
+
   Array.from(navImages).forEach((img) =>
-    img.classList.toggle("menu-collapsed")
+    img.classList.toggle("menu-collapsed"),
   );
   window.addEventListener("DOMContentLoaded", () => {
     collapse();
+  });
+
+  const firstFocusableElement = document.getElementById("burger-button");
+  const lastFocusableElement =
+    document.getElementById("menu").lastElementChild;
+
+  document.addEventListener("keydown", function (e) {
+    const activeElement = document.activeElement.textContent;
+    if (e.key === "Tab") {
+      if (e.shiftKey) {
+        if (document.activeElement === firstFocusableElement) {
+          e.preventDefault();
+          lastFocusableElement.focus();
+        }
+      } else {
+        if (activeElement == "CONTACT") {
+          e.preventDefault();
+          firstFocusableElement.focus();
+        }
+      }
+    }
   });
 };
 
